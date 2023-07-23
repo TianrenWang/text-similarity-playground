8ed1fec331386cb0a6f7c098ba827fda78a44b2b
Frank Wang <broodstar_frank@hotmail.com>
2023-07-23 12:00:56 -0400

    added useState code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from "react";
 import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import { Box, Button, TextField } from "@mui/material";
+import Vectors from "./components/Vectors";
+
+const textfieldStyle = { width: 200, margin: 1 };
 
 export default function App() {
+  const [openAISecret, setOpenAISecret] = useState(
+    window.localStorage.getItem("openAISecret") || ""
+  );
+  const [pineconeAPIKey, setPineconeAPIKey] = useState(
+    window.localStorage.getItem("pineconeAPIKey") || ""
+  );
+  const [pineconeIndex, setPineconeIndex] = useState(
+    window.localStorage.getItem("pineconeIndex") || ""
+  );
+  const [namespace, setNamespace] = useState(
+    window.localStorage.getItem("namespace") || ""
+  );
+  const [pineconeEnvironment, setPineconeEnvironment] = useState(
+    window.localStorage.getItem("pineconeEnvironment") || ""
+  );
+  const [startedSession, setStartedSession] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
